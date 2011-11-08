ea4f0c2d0f0bea6c142ca8842fe33bfcc335910c
Zach Dwiel <zdwiel@plotwatt.com>
2011-11-08 09:14:22 -0500

    tabs to spaces

--- scripts/Parser_test.py
+++ scripts/Parser_test.py
@@ -20,258 +20,258 @@ n.bind('test', '<http://dwiel.net/express/test/0.1/>')
 n.bind('flickr', '<http://dwiel.net/express/flickr/0.1/>')
 
 class PassCompleteReadsTestCase(unittest.TestCase):
-	def setUp(self):
-		self.parser = Parser(n)
-	
-	def test1(self):
-		assert self.parser.parse_expression("image[flickr.tag] = 'sunset'") == [[n.var.image, n.flickr.tag, 'sunset']]
-	
-	def test1b(self):
-		assert self.parser.parse_expression("image[flickr.tag] = 'sunset' | 'sunrise'") == [[n.var.image, n.flickr.tag, ['sunset', 'sunrise']]]
-	
-	def test2(self):
-		assert self.parser.parse_expression("image[flickr:tag] = 1.5") == [[n.var.image, n.flickr.tag, 1.5]]
-	
-	def test3(self):
-		assert self.parser.parse_expression("image[flickr:tag] = 4 * 9") == [[n.var.image, n.flickr.tag, 4 * 9]]
+  def setUp(self):
+    self.parser = Parser(n)
+  
+  def test1(self):
+    assert self.parser.parse_expression("image[flickr.tag] = 'sunset'") == [[n.var.image, n.flickr.tag, 'sunset']]
+  
+  def test1b(self):
+    assert self.parser.parse_expression("image[flickr.tag] = 'sunset' | 'sunrise'") == [[n.var.image, n.flickr.tag, ['sunset', 'sunrise']]]
+  
+  def test2(self):
+    assert self.parser.parse_expression("image[flickr:tag] = 1.5") == [[n.var.image, n.flickr.tag, 1.5]]
+  
+  def test3(self):
+    assert self.parser.parse_expression("image[flickr:tag] = 4 * 9") == [[n.var.image, n.flickr.tag, 4 * 9]]
 
-	def test4(self):
-		assert self.parser.parse_expression("image[flickr.tag] = tag") == [[n.var.image, n.flickr.tag, n.var.tag]]
-	
-	def test5(self):
-		assert self.parser.parse_expression("tag = image[flickr.tag]") == [[n.var.image, n.flickr.tag, n.var.tag]]
-	
-	def test6(self):
-		assert self.parser.parse_expression("image1[flickr.tag] = image2[flickr.tag]") == [
-			[n.var.image1, n.flickr.tag, n.var.bnode1],
-			[n.var.image2, n.flickr.tag, n.var.bnode1],
-		]
-	
-	def test7(self):
-		assert self.parser.parse_expression("tag = image[flickr.tag][string.upper]") == [
-			[n.var.image, n.flickr.tag, n.var.bnode1],
-			[n.var.bnode1, n.string['upper'], n.var.tag],
-		]
-	
-	def test8(self):
-		assert self.parser.parse_expression("""
-			color.distance{
-				color.rgb : 1,
-				color.rgb : 2,
-			} = distance""") == [
-			[n.var.bnode1, n.color.rgb, 1],
-			[n.var.bnode1, n.color.rgb, 2],
-			[n.var.bnode1, n.color.distance, n.var.distance],
-		]
-	
-	def test9(self):
-		assert self.parser.parse_expression("""
-			math.sum(1, 2) = sum""") == [
-			[n.var.bnode1, n.call.arg1, 1],
-			[n.var.bnode1, n.call.arg2, 2],
-			[n.var.bnode1, n.math.sum, n.var.sum],
-		]
-	
-	#def test10(self):
-		#assert self.parser.parse_expression("""
-			#color.distance{
-				#color.rgb : something[color],
-				#color.rgb : 2,
-			#} = distance""") == [
-			#[n.var.something, n.var.color, n.var.bnode2],
-			#[n.var.bnode1, n.color.rgb, n.var.bnode2],
-			#[n.var.bnode1, n.color.rgb, 2],
-			#[n.var.bnode1, n.color.distance, n.var.distance],
-		#]
-	
-	def test11(self):
-		assert self.parser.parse_expression("image[flickr.tag] = x") == [[n.var.image, n.flickr.tag, n.var.x]]
+  def test4(self):
+    assert self.parser.parse_expression("image[flickr.tag] = tag") == [[n.var.image, n.flickr.tag, n.var.tag]]
+  
+  def test5(self):
+    assert self.parser.parse_expression("tag = image[flickr.tag]") == [[n.var.image, n.flickr.tag, n.var.tag]]
+  
+  def test6(self):
+    assert self.parser.parse_expression("image1[flickr.tag] = image2[flickr.tag]") == [
+      [n.var.image1, n.flickr.tag, n.var.bnode1],
+      [n.var.image2, n.flickr.tag, n.var.bnode1],
+    ]
+  
+  def test7(self):
+    assert self.parser.parse_expression("tag = image[flickr.tag][string.upper]") == [
+      [n.var.image, n.flickr.tag, n.var.bnode1],
+      [n.var.bnode1, n.string['upper'], n.var.tag],
+    ]
+  
+  def test8(self):
+    assert self.parser.parse_expression("""
+      color.distance{
+        color.rgb : 1,
+        color.rgb : 2,
+      } = distance""") == [
+      [n.var.bnode1, n.color.rgb, 1],
+      [n.var.bnode1, n.color.rgb, 2],
+      [n.var.bnode1, n.color.distance, n.var.distance],
+    ]
+  
+  def test9(self):
+    assert self.parser.parse_expression("""
+      math.sum(1, 2) = sum""") == [
+      [n.var.bnode1, n.call.arg1, 1],
+      [n.var.bnode1, n.call.arg2, 2],
+      [n.var.bnode1, n.math.sum, n.var.sum],
+    ]
+  
+  #def test10(self):
+    #assert self.parser.parse_expression("""
+      #color.distance{
+        #color.rgb : something[color],
+        #color.rgb : 2,
+      #} = distance""") == [
+      #[n.var.something, n.var.color, n.var.bnode2],
+      #[n.var.bnode1, n.color.rgb, n.var.bnode2],
+      #[n.var.bnode1, n.color.rgb, 2],
+      #[n.var.bnode1, n.color.distance, n.var.distance],
+    #]
+  
+  def test11(self):
+    assert self.parser.parse_expression("image[flickr.tag] = x") == [[n.var.image, n.flickr.tag, n.var.x]]
 
-	def test12(self):
-		assert self.parser.parse_expression("image[flickr:tag] = True") == [[n.var.image, n.flickr.tag, True]]
-	
-	def test13(self):
-		assert self.parser.parse_expression("image[flickr.tag] = image_tag") == [[n.var.image, n.flickr.tag, n.var.image_tag]]
-		
-	def test14(self):
-		assert self.parser.parse_expression('image[file.filename] = "/home/dwiel/AMOSvid/1065/20080821_083129.jpg"') == [[n.var.image, n.file.filename, "/home/dwiel/AMOSvid/1065/20080821_083129.jpg"]]
-		
-	def test15(self):
-		assert self.parser.parse_expression('image[file.filename] = "home/dwiel/AMOSvid/1065/*.jpg"[glob.glob]') == [
-			[n.var.image, n.file.filename, n.var.bnode1],
-			["home/dwiel/AMOSvid/1065/*.jpg", n.glob.glob, n.var.bnode1],
-		]
-	
-	def test16(self):
-		assert self.parser.parse_expression('_pattern[glob.glob] = ?filename') == [
-			[n.lit_var.pattern, n.glob.glob, n.meta_var.filename],
-		]
-	
-	def test17(self):
-		query = """uri[test.result] = '<script type="text/javascript" src="external.js"></script>'"""
-		assert self.parser.parse_query(query) == [
-			[ n.var.uri, n.test.result, '<script type="text/javascript" src="external.js"></script>', ],
-		]
-		
-	def test18(self):
-		query = """test.func("xyz()", 'abc = 123') = "what's um, the deal?" """
-		assert self.parser.parse_query(query) == [
-			[ n.var.bnode1, n.call.arg1, 'xyz()', ],
-			[ n.var.bnode1, n.call.arg2, 'abc = 123', ],
-			[ n.var.bnode1, n.test.func, "what's um, the deal?", ],
-		]
-	
-	def test19(self):
-		query = """test.func('''xyz() + 'what?' and "what?" ''', 'abc = 123') = "what's um, the deal?" """
-		assert self.parser.parse_query(query) == [
-			[ n.var.bnode1, n.call.arg1, 'xyz() + \'what?\' and "what?" ', ],
-			[ n.var.bnode1, n.call.arg2, 'abc = 123', ],
-			[ n.var.bnode1, n.test.func, "what's um, the deal?", ],
-		]
-	
-	def test_parseQuery1(self):
-		query = [
-			'uri[test.sum] = sum',
-		]
-		assert self.parser.parse_query(query) == [
-			[n.var.uri, n.test.sum, n.var.sum],
-		]
-	
-	def test_parseQuery2(self):
-		query = [
-			'uri[test.sum] = sum',
-			'uri[test.x] = uri2[test.x]',
-			[n.var.uri, n.test.x, 1],
-		]
-		assert self.parser.parse_query(query) == [
-			[n.var.uri, n.test.sum, n.var.sum],
-			[n.var.uri, n.test.x, n.var.bnode1],
-			[n.var.uri2, n.test.x, n.var.bnode1],
-			[n.var.uri, n.test.x, 1],
-		]
-	
-	def test_parseQuery3(self):
-		query = [
-			'image[file.filename] = "/home/dwiel/pictures/stitt blanket/*.jpg"[glob.glob]',
-			'thumb = image.thumbnail(image, 4, 4, image.antialias)',
-			'thumb_image = thumb[pil.image]',
-		]
-		ret = self.parser.parse_query(query)
-		assert ret == [
-			[ n.var.image, n.file.filename, n.var.bnode1, ],
-			[ '/home/dwiel/pictures/stitt blanket/*.jpg', n.glob.glob, n.var.bnode1, ],
-			[ n.var.bnode2, n.call.arg1, n.var.image, ],
-			[ n.var.bnode2, n.call.arg2, 4, ],
-			[ n.var.bnode2, n.call.arg3, 4, ],
-			[ n.var.bnode2, n.call.arg4, n.image.antialias, ],
-			[ n.var.bnode2, n.image.thumbnail, n.var.thumb, ],
-			[ n.var.thumb, n.pil.image, n.var.thumb_image, ],
-		]
-	
-	def test_parseQuery4(self):
-		query = """
-			uri[test.sum] = sum
-			uri[test.x] = uri2[test.x]
-			uri[test.x] = 1
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var.uri, n.test.sum, n.var.sum],
-			[n.var.uri, n.test.x, n.var.bnode1],
-			[n.var.uri2, n.test.x, n.var.bnode1],
-			[n.var.uri, n.test.x, 1],
-		]
-	
-	def test_parseKeyword(self):
-		query = """
-			count[file.count] = _count
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var['count'], n.file['count'], n.lit_var['count']],
-		]
-	
-	def test_parseBrokenQuery(self):
-		query = """
-			note[e.tag] _tag
-		"""
-		try :
-			p('parsed',self.parser.parse_query(query))
-		except TypeError :
-			assert True
-	
-	def test_emptyNamespace(self):
-		query = """
-			note[:tag] = _tag
-		"""
-		
-		assert self.parser.parse_query(query) == [
-			[n.var['note'], n['']['tag'], n.lit_var['tag']],
-		]
-	
-	def test_emptyNamespace2(self):
-		query = """
-			note[.tag] = _tag
-		"""
-		
-		assert self.parser.parse_query(query) == [
-			[n.var['note'], n['']['tag'], n.lit_var['tag']],
-		]
-	
-	def test_URIwithSlashes(self):
-		query = """
-			x[test./type/type] = test.property
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var['x'], n.test['/type/type'], n.test['property']],
-		]
-		
-	def test_full_line_comment(self):
-		query = """
-			x[test./type/type] = test.property
-			# this is a comment
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var['x'], n.test['/type/type'], n.test['property']],
-		]
-		
-	def test_after_triple_comment(self):
-		query = """
-			x[test./type/type] = test.property # this is a comment
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var['x'], n.test['/type/type'], n.test['property']],
-		]
-	
-	def test_comment_in_middle_of_query(self):
-		query = """
-			x[test./type/type] = test.property
-			# this is a comment
-			x[test./type/type] = test.property
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var['x'], n.test['/type/type'], n.test['property']],
-			[n.var['x'], n.test['/type/type'], n.test['property']],
-		]
-	
-	def test_implied_var(self):
-		query = """
-			x[test.test][test.test] = 1
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var['x'], n.test['test'], n.var['bnode1']],
-			[n.var['bnode1'], n.test['test'], 1]
-		]
-	
-	def test_multiLineOr(self):
-		query = """
-			i[flickr.tag] = 'sunset' |
-			                'sunrise'
-		"""
-		assert self.parser.parse_query(query) == [
-			[n.var['i'], n['flickr']['tag'], ['sunset', 'sunrise']],
-		]
-	
+  def test12(self):
+    assert self.parser.parse_expression("image[flickr:tag] = True") == [[n.var.image, n.flickr.tag, True]]
+  
+  def test13(self):
+    assert self.parser.parse_expression("image[flickr.tag] = image_tag") == [[n.var.image, n.flickr.tag, n.var.image_tag]]
+    
+  def test14(self):
+    assert self.parser.parse_expression('image[file.filename] = "/home/dwiel/AMOSvid/1065/20080821_083129.jpg"') == [[n.var.image, n.file.filename, "/home/dwiel/AMOSvid/1065/20080821_083129.jpg"]]
+    
+  def test15(self):
+    assert self.parser.parse_expression('image[file.filename] = "home/dwiel/AMOSvid/1065/*.jpg"[glob.glob]') == [
+      [n.var.image, n.file.filename, n.var.bnode1],
+      ["home/dwiel/AMOSvid/1065/*.jpg", n.glob.glob, n.var.bnode1],
+    ]
+  
+  def test16(self):
+    assert self.parser.parse_expression('_pattern[glob.glob] = ?filename') == [
+      [n.lit_var.pattern, n.glob.glob, n.meta_var.filename],
+    ]
+  
+  def test17(self):
+    query = """uri[test.result] = '<script type="text/javascript" src="external.js"></script>'"""
+    assert self.parser.parse_query(query) == [
+      [ n.var.uri, n.test.result, '<script type="text/javascript" src="external.js"></script>', ],
+    ]
+    
+  def test18(self):
+    query = """test.func("xyz()", 'abc = 123') = "what's um, the deal?" """
+    assert self.parser.parse_query(query) == [
+      [ n.var.bnode1, n.call.arg1, 'xyz()', ],
+      [ n.var.bnode1, n.call.arg2, 'abc = 123', ],
+      [ n.var.bnode1, n.test.func, "what's um, the deal?", ],
+    ]
+  
+  def test19(self):
+    query = """test.func('''xyz() + 'what?' and "what?" ''', 'abc = 123') = "what's um, the deal?" """
+    assert self.parser.parse_query(query) == [
+      [ n.var.bnode1, n.call.arg1, 'xyz() + \'what?\' and "what?" ', ],
+      [ n.var.bnode1, n.call.arg2, 'abc = 123', ],
+      [ n.var.bnode1, n.test.func, "what's um, the deal?", ],
+    ]
+  
+  def test_parseQuery1(self):
+    query = [
+      'uri[test.sum] = sum',
+    ]
+    assert self.parser.parse_query(query) == [
+      [n.var.uri, n.test.sum, n.var.sum],
+    ]
+  
+  def test_parseQuery2(self):
+    query = [
+      'uri[test.sum] = sum',
+      'uri[test.x] = uri2[test.x]',
+      [n.var.uri, n.test.x, 1],
+    ]
+    assert self.parser.parse_query(query) == [
+      [n.var.uri, n.test.sum, n.var.sum],
+      [n.var.uri, n.test.x, n.var.bnode1],
+      [n.var.uri2, n.test.x, n.var.bnode1],
+      [n.var.uri, n.test.x, 1],
+    ]
+  
+  def test_parseQuery3(self):
+    query = [
+      'image[file.filename] = "/home/dwiel/pictures/stitt blanket/*.jpg"[glob.glob]',
+      'thumb = image.thumbnail(image, 4, 4, image.antialias)',
+      'thumb_image = thumb[pil.image]',
+    ]
+    ret = self.parser.parse_query(query)
+    assert ret == [
+      [ n.var.image, n.file.filename, n.var.bnode1, ],
+      [ '/home/dwiel/pictures/stitt blanket/*.jpg', n.glob.glob, n.var.bnode1, ],
+      [ n.var.bnode2, n.call.arg1, n.var.image, ],
+      [ n.var.bnode2, n.call.arg2, 4, ],
+      [ n.var.bnode2, n.call.arg3, 4, ],
+      [ n.var.bnode2, n.call.arg4, n.image.antialias, ],
+      [ n.var.bnode2, n.image.thumbnail, n.var.thumb, ],
+      [ n.var.thumb, n.pil.image, n.var.thumb_image, ],
+    ]
+  
+  def test_parseQuery4(self):
+    query = """
+      uri[test.sum] = sum
+      uri[test.x] = uri2[test.x]
+      uri[test.x] = 1
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var.uri, n.test.sum, n.var.sum],
+      [n.var.uri, n.test.x, n.var.bnode1],
+      [n.var.uri2, n.test.x, n.var.bnode1],
+      [n.var.uri, n.test.x, 1],
+    ]
+  
+  def test_parseKeyword(self):
+    query = """
+      count[file.count] = _count
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var['count'], n.file['count'], n.lit_var['count']],
+    ]
+  
+  def test_parseBrokenQuery(self):
+    query = """
+      note[e.tag] _tag
+    """
+    try :
+      p('parsed',self.parser.parse_query(query))
+    except TypeError :
+      assert True
+  
+  def test_emptyNamespace(self):
+    query = """
+      note[:tag] = _tag
+    """
+    
+    assert self.parser.parse_query(query) == [
+      [n.var['note'], n['']['tag'], n.lit_var['tag']],
+    ]
+  
+  def test_emptyNamespace2(self):
+    query = """
+      note[.tag] = _tag
+    """
+    
+    assert self.parser.parse_query(query) == [
+      [n.var['note'], n['']['tag'], n.lit_var['tag']],
+    ]
+  
+  def test_URIwithSlashes(self):
+    query = """
+      x[test./type/type] = test.property
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var['x'], n.test['/type/type'], n.test['property']],
+    ]
+    
+  def test_full_line_comment(self):
+    query = """
+      x[test./type/type] = test.property
+      # this is a comment
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var['x'], n.test['/type/type'], n.test['property']],
+    ]
+    
+  def test_after_triple_comment(self):
+    query = """
+      x[test./type/type] = test.property # this is a comment
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var['x'], n.test['/type/type'], n.test['property']],
+    ]
+  
+  def test_comment_in_middle_of_query(self):
+    query = """
+      x[test./type/type] = test.property
+      # this is a comment
+      x[test./type/type] = test.property
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var['x'], n.test['/type/type'], n.test['property']],
+      [n.var['x'], n.test['/type/type'], n.test['property']],
+    ]
+  
+  def test_implied_var(self):
+    query = """
+      x[test.test][test.test] = 1
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var['x'], n.test['test'], n.var['bnode1']],
+      [n.var['bnode1'], n.test['test'], 1]
+    ]
+  
+  def test_multiLineOr(self):
+    query = """
+      i[flickr.tag] = 'sunset' |
+                      'sunrise'
+    """
+    assert self.parser.parse_query(query) == [
+      [n.var['i'], n['flickr']['tag'], ['sunset', 'sunrise']],
+    ]
+  
 if __name__ == "__main__" :
-	unittest.main()
+  unittest.main()
 
 
 #print parse("image[image.average_color] = color")
