aef4e1bca9ef1a7d95d2fc17ac1413b9dbc48891
Zach Dwiel <zdwiel@plotwatt.com>
2012-06-25 14:32:02 -0400

    add yelp translation

---
+++ translations/yelp.py
@@ -0,0 +1,14 @@
+"pizza"
+"beer and buffalo wings"
+"nearby stores"
+"pizaa places nearby"
+"sports stores"
+"near by italian"
+"where can I find good italian food"
+"where do you recommend I go for mexican"
+"where can I find a quick burrito"
+
+def loadTranslations(axpress) :
+  axpress.n.bind('yelp', '<http://dwiel.net/axpress/yelp/0.1/>')
+
+  
