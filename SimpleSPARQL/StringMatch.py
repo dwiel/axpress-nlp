e28b51ee7a6eeebfe38f81a3d0ccb266d6065e8e
Zach Dwiel <zdwiel@plotwatt.com>
2013-01-02 19:50:57 -0500

    fix bugs in string matcher

--- SimpleSPARQL/StringMatch.py
+++ SimpleSPARQL/StringMatch.py
@@ -1,4 +1,10 @@
 import re
+import json
+
+"""
+Why I'm not using regexp 'natively':
+http://stackoverflow.com/questions/4091634/finding-all-matches-with-a-regular-expression-greedy-and-non-greedy
+"""
 
 split_re = re.compile('%[\w\.:_]+%')
 def split_string(s) :
@@ -15,10 +21,13 @@ def split_string(s) :
     [p[1:-1] for p in split_re.findall(s)]
   )
 
-def _match(str, const) :
+def _match(str, const, allow_first_empty) :
   """
   @arg str the string to match against
   @arg const is a list of constant substring to split the str from
+  @arg allow_first_empty should be true when the first const is right up 
+       against the start, allowing the first 'empty' variable match to be 
+       empty and ignored.
   @return generate tuples of substrings from str that fit between the consts
   Note that this function is recursive and recurs by findings all substring 
     that come before const[0] and then recuring on the rest of the string
@@ -41,13 +50,16 @@ def _match(str, const) :
       # if the c string was found right at the front of the string, then
       # front will be empty and we don't want to bind a var to an empty
       # string
-      if not front :
+      if not front and not allow_first_empty :
         continue
       
-      # we split based on the firts known constant substring, now look 
+      # we split based on the first known constant substring, now look 
       # for the rest of the splits
-      for m in _match(back,  const[1:]) :
-        yield (front,) + m
+      for m in _match(back, const[1:], False) :
+        if front :
+          yield (front,) + m
+        else :
+          yield m
 
 def match(pat, str) :
   """
@@ -63,6 +75,9 @@ def match(pat, str) :
   # remove empty string const values at the end of begenning of the list
   if const[0] == '' :
     const = const[1:]
+    allow_first_empty = False
+  else :
+    allow_first_empty = True
   if const[-1] == '' :
     const = const[:-1]
 
@@ -76,7 +91,7 @@ def match(pat, str) :
   
   # if there were some matches return them, if there were no matches
   # return False
-  matches = list(_match(str, const))
+  matches = list(_match(str, const, allow_first_empty))
   if matches :
     ms = []
     for m in matches :
@@ -85,35 +100,69 @@ def match(pat, str) :
   else :
     return False
 
-assert match("%x%o%y%", "dogod") == [
-  {'x' : 'd', 'y' : 'god'},
-  {'x' : 'dog', 'y' : 'd'},
-]
-    
-assert match("%a%XY%b%YZ%c%", "abcXYdefXYghYZijYZkl") == [
-  {'a' : 'abc', 'b' : 'defXYgh', 'c' : 'ijYZkl'},
-  {'a' : 'abc', 'b' : 'defXYghYZij', 'c' : 'kl'},
-  {'a' : 'abcXYdef', 'b' : 'gh', 'c' : 'ijYZkl'},
-  {'a' : 'abcXYdef', 'b' : 'ghYZij', 'c' : 'kl'},
-]
-assert match("%a%XY%b%YZ%c%", "XYdefXYghYZijYZkl") == [
-  {'a' : 'XYdef', 'b' : 'gh', 'c' : 'ijYZkl'},
-  {'a' : 'XYdef', 'b' : 'ghYZij', 'c' : 'kl'},
-]
-assert match("%a%%b%", "abc") == [
-  {'a' : 'a', 'b' : 'bc'},
-  {'a' : 'ab', 'b' : 'c'},
-]
-assert match('abc', 'abc') == []
-assert match('abcdef', 'abc') == False
-assert match("%x%o%y%", "digid") == False
-assert match('%x%', 'abc') == [{'x' : 'abc'}]
+if __name__ == '__main__' :
+  ret = match("%x%o%y%", "dogod")
+  #print ret
+  assert ret == [
+    {'x' : 'd', 'y' : 'god'},
+    {'x' : 'dog', 'y' : 'd'},
+  ]
+      
+  assert match("%a%XY%b%YZ%c%", "abcXYdefXYghYZijYZkl") == [
+    {'a' : 'abc', 'b' : 'defXYgh', 'c' : 'ijYZkl'},
+    {'a' : 'abc', 'b' : 'defXYghYZij', 'c' : 'kl'},
+    {'a' : 'abcXYdef', 'b' : 'gh', 'c' : 'ijYZkl'},
+    {'a' : 'abcXYdef', 'b' : 'ghYZij', 'c' : 'kl'},
+  ]
+  ret = match("%a%XY%b%YZ%c%", "XYdefXYghYZijYZkl")
+  #print 'ret', json.dumps(ret, indent=4)
+  assert ret == [
+    {'a' : 'XYdef', 'b' : 'gh', 'c' : 'ijYZkl'},
+    {'a' : 'XYdef', 'b' : 'ghYZij', 'c' : 'kl'},
+  ]
+  assert match("%a%%b%", "abc") == [
+    {'a' : 'a', 'b' : 'bc'},
+    {'a' : 'ab', 'b' : 'c'},
+  ]
+  assert match('abc', 'abc') == []
+  assert match('abcdef', 'abc') == False
+  assert match("%x%o%y%", "digid") == False
+  assert match('%x%', 'abc') == [{'x' : 'abc'}]
 
-# the constant part of a pattern can be arbitrary regexp:
-assert match("%time_s% (on |in |)%date_s%", "5 on friday") == [
-  {'time_s' : '5', 'date_s' : 'friday'},
-]
-assert match("%time_s% (on |in |)%date_s%", "5 friday") == [
-  {'time_s' : '5', 'date_s' : 'friday'},
-]
+  # the constant part of a pattern can be arbitrary regexp:
+  assert match("%time_s% (on |in |)%date_s%", "5 on friday") == [
+    {'time_s' : '5', 'date_s' : 'friday'},
+  ]
+  assert match("%time_s% (on |in |)%date_s%", "5 friday") == [
+    {'time_s' : '5', 'date_s' : 'friday'},
+  ]
 
+  ret = match("x %a%", "x y z")
+  #print ret
+  assert ret == [
+    {'a' : 'y z'},
+  ]
+
+  assert match("x %a% %b%", "x y z w") == [
+    {'a' : 'y', 'b' : 'z w'},
+    {'a' : 'y z', 'b' : 'w'},
+  ]
+
+  assert match("remind me to %msg% %dt%", "remind me to go to keleinforfers tomorrow at 5 pm") == [
+    {'msg': 'go', 'dt': 'to keleinforfers tomorrow at 5 pm'},
+    {'msg': 'go to', 'dt': 'keleinforfers tomorrow at 5 pm'},
+    {'msg': 'go to keleinforfers', 'dt': 'tomorrow at 5 pm'},
+    {'msg': 'go to keleinforfers tomorrow', 'dt': 'at 5 pm'},
+    {'msg': 'go to keleinforfers tomorrow at', 'dt': '5 pm'},
+    {'msg': 'go to keleinforfers tomorrow at 5', 'dt': 'pm'}
+  ]
+  
+  ret = match("remind me to %msg% (at |)%dt%", "remind me to go to keleinforfers tomorrow at 5 pm")
+  #print 'ret', json.dumps(ret, indent=4)
+  assert ret == [
+    {'msg': 'go', 'dt': 'to keleinforfers tomorrow at 5 pm'},
+    {'msg': 'go to', 'dt': 'keleinforfers tomorrow at 5 pm'},
+    {'msg': 'go to keleinforfers', 'dt': 'tomorrow at 5 pm'},
+    {'msg': 'go to keleinforfers tomorrow', 'dt': '5 pm'},
+    {'msg': 'go to keleinforfers tomorrow at 5', 'dt': 'pm'}
+  ]
