b15da22c6d18ce66daa1d43ad0b67623721c83ef
Zach Dwiel <zdwiel@plotwatt.com>
2013-02-10 19:56:50 -0500

    cleanup AxpressTestCase init

--- scripts/AxpressTestCase.py
+++ scripts/AxpressTestCase.py
@@ -6,80 +6,15 @@ import time, urllib
 from rdflib import *
 from SimpleSPARQL import *
 
-sparql = SimpleSPARQL("http://localhost:2020/sparql")
-sparql.setGraph("http://dwiel.net/axpress/testing")
-
-n = sparql.n
-n.bind('string', '<http://dwiel.net/express/string/0.1/>')
-n.bind('math', '<http://dwiel.net/express/math/0.1/>')
-n.bind('file', '<http://dwiel.net/express/file/0.1/>')
-n.bind('glob', '<http://dwiel.net/express/glob/0.1/>')
-n.bind('color', '<http://dwiel.net/express/color/0.1/>')
-n.bind('sparql', '<http://dwiel.net/express/sparql/0.1/>')
-n.bind('call', '<http://dwiel.net/express/call/0.1/>')
-n.bind('library', '<http://dwiel.net/axpress/library/0.1/>')
-n.bind('music', '<http://dwiel.net/axpress/music/0.1/>')
-n.bind('music_album', '<http://dwiel.net/axpress/music_album/0.1/>')
-n.bind('source', '<http://dwiel.net/axpress/source/0.1/>')
-n.bind('lastfm', '<http://dwiel.net/axpress/lastfm/0.1/>')
-n.bind('rdfs', '<http://www.w3.org/2000/01/rdf-schema#>')
-n.bind('bound_var', '<http://dwiel.net/axpress/bound_var/0.1/>')
-n.bind('flickr', '<http://dwiel.net/axpress/flickr/0.1/>')
-n.bind('amos', '<http://dwiel.net/axpress/amos/0.1/>')
-n.bind('test', '<http://dwiel.net/express/test/0.1/>')
-n.bind('freebase', '<http://dwiel.net/axpress/freebase/0.1/>')
-n.bind('wunderground', '<http://dwiel.net/axpress/wunderground/0.1/>')
-a = n.rdfs.type
-
 # for easy basic stupid matching type instance
 class X():pass
 type_instance = type(X())
 
-axpress = Axpress(sparql = sparql)
+axpress = Axpress()
 axpress.compiler.debug_off()
-loadTranslations(axpress, n)
-
+loadTranslations(axpress)
 
 class AxpressTestCase(unittest.TestCase):
-  #def setUp(self):
-    #pass
-
-  # depends on Joseki
-  #def testIncomingBindings(self):
-    #def is_num(x):
-      #return isinstance(x, (int, long, float))
-    
-    #bindings_set = axpress.read_sparql("""
-      #foo[test.x] = x
-      #foo[test.y] = y
-    #""")
-    
-    ## reduce bindings to those whose x and y values are numbers
-    ## I'd like this functionality to exist, but I don't think this is the right
-    ## way to get at it, or think about it.  For one, the type should take care
-    ## of most of this kind of thing - though I know that it shouldn't be relied
-    ## on ...
-    ##bindings_set = [x for x in bindings_set]
-    ##print 'bindings_set',prettyquery(bindings_set)
-    #new_bindings_set = []
-    #for bindings in bindings_set :
-      #if is_num(bindings['x']) and is_num(bindings['y']) :
-        #new_bindings_set.append(bindings)
-    #bindings_set = new_bindings_set
-    ##print 'bindings_set',prettyquery(bindings_set)
-    
-    #ret = axpress.read_translate("""
-      #foo[test.x] = x
-      #foo[test.y] = y
-      #foo[test.sum] = _sum
-    #""", reqd_bound_vars = ['sum'], bindings_set = bindings_set)
-    ##p('ret',ret)
-    #assert ret == [
-      #{
-        #'sum' : 3,
-      #},
-    #]
-  
   def testTranslationReturnsMultipleValues(self):
     """ note, at one time, thumb was included in the reqd_bound_vars, but
     I'm not really sure why.  It doesn't really have any meaning.
@@ -606,14 +541,7 @@ class AxpressTestCase(unittest.TestCase):
       pass
     
   def testMultiBranchJoin(self):
-    compiler = Compiler(n)
-    evaluator = Evaluator(n)
-
-    axpress = Axpress(
-      sparql = sparql,
-      compiler = compiler,
-      evaluator = evaluator
-    )
+    axpress = Axpress()
     
     def make_rule(name, i, o, fn = None):
       axpress.register_translation({
